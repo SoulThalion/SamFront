30d50e3def34285e7d6bd2b820e05672b02ff76f
SoulThalion <adrielguijarro@gmail.com>
2024-04-29 23:51:56 +0100

    users en proceso

--- SamFront/src/pages/Users.jsx
+++ SamFront/src/pages/Users.jsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from "react";
 import { getAllUsers } from "../services/users.service";
+import UsersTable from "../components/UsersTable";
 
 const Users = () => {
-
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     const fetchAllUsers = async () => {
-      const data = await getAllUsers()
-      console.log(data)
-      setUsers(data)
-    }
+      const data = await getAllUsers();
+      console.log(data);
+      setUsers(data);
+    };
 
-    fetchAllUsers()
-
-  }, [])
+    fetchAllUsers();
+  }, []);
 
   return (
-    <>
-      <div className="flex justify-center items-center h-screen text-white" style={{ backgroundColor: "#1c1d20" }}>
-        <h1 className="text-4xl text-center mb-4">Users</h1>
-        <div>{users}</div>
-      </div>
-    </>
+    <div className="text-white" style={{ backgroundColor: "#1c1d20" }}>
+      
+      <div className="flex justify-center items-center h-screen text-white">
+        <UsersTable />
+      </div><h1 className="absolute top-20 right-5 text-4xl text-center mb-4 z-100">Users</h1>
+    </div>
   );
 };
 
